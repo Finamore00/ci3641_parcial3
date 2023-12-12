566a7c6038bee85651d8f50c440a7390f7721311
Santiago Finamore <18-10125@usb.ve>
2023-12-12 17:59:37 -0400

    Nueva implementación de Pregunta 7 en C++

---
+++ Pregunta 7/wadefoc.cpp
@@ -0,0 +1,31 @@
+#include <boost/multiprecision/gmp.hpp>
+#include <boost/math/special_functions/log1p.hpp>
+#include <iostream>
+
+using namespace boost::multiprecision;
+using namespace std;
+
+mpz_int fib_aux(mpz_int x, mpz_int y, mpz_int k) {
+    mpz_int p_res;
+    return (k == 0 ? 0 : (k == 1 ? x : fib_aux(x+y, x, k-1)));
+}
+
+mpz_int fibonacci(mpz_int n) {
+    return fib_aux(1, 0, n);
+}
+
+mpf_float my_log2(mpf_float x) {
+    return boost::math::log1p(x)/boost::math::log1p(1);
+}
+
+mpz_int wadefoc(mpz_int n) {
+    mpf_float nf = n.convert_to<boost::multiprecision::mpf_float>();
+    mpf_float narayana = (((nf+1)*nf*nf*(n-1))/12);
+    mpz_int l2 = my_log2(narayana + 1).convert_to<mpz_int>();
+    return fibonacci(l2 + 1);
+}
+
+int main(int argC, char* argV[]) {
+    mpz_int n = mpz_int(argV[1]);
+    cout << wadefoc(n) << endl;
+}
