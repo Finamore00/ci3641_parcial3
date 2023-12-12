e9a18981a177bc846b98228c367e14724e58c139
Santiago Finamore <18-10125@usb.ve>
2023-12-12 18:30:32 -0400

    Corregido error en log2. Pregunta 7

--- Pregunta 7/wadefoc.cpp
+++ Pregunta 7/wadefoc.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 
 using namespace boost::multiprecision;
-using namespace std;
 
 mpz_int fib_aux(mpz_int x, mpz_int y, mpz_int k) {
     mpz_int p_res;
@@ -15,17 +14,17 @@ mpz_int fibonacci(mpz_int n) {
 }
 
 mpf_float my_log2(mpf_float x) {
-    return boost::math::log1p(x)/boost::math::log1p(1);
+    return boost::math::log1p(x-1)/boost::math::log1p(1);
 }
 
 mpz_int wadefoc(mpz_int n) {
     mpf_float nf = n.convert_to<boost::multiprecision::mpf_float>();
     mpf_float narayana = (((nf+1)*nf*nf*(n-1))/12);
-    mpz_int l2 = my_log2(narayana + 1).convert_to<mpz_int>();
+    mpz_int l2 = my_log2(narayana + 1).convert_to<mpz_int>(); //Takes floor by converting
     return fibonacci(l2 + 1);
 }
 
 int main(int argC, char* argV[]) {
     mpz_int n = mpz_int(argV[1]);
-    cout << wadefoc(n) << endl;
+    std::cout << wadefoc(n) << std::endl;
 }
